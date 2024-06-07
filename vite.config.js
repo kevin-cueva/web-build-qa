0c5ee9d6f02b1b2f5314532911e32a810be82af9
Kevin Cueva <kevinneillcuevaalmendrales@gmail.com>
2024-06-07 13:06:25 -0500

    Corregir

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  page: "https://kevin-cueva.github.io/web-build-qa",
+  base: "https://kevin-cueva.github.io/web-build-qa",
 })
