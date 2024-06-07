d6eef3d793ceb40719e14d4bcbfdf6418239e32d
Kevin Cueva <kevinneillcuevaalmendrales@gmail.com>
2024-06-07 13:02:44 -0500

    ruta

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  page: "https://kevin-cueva.github.io/web-build-qa",
 })
